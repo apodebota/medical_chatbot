02a934ea933ca3fbfd0076c75f8a6af2914bad50
ar98365 <ar98365@gmail.com>
2025-08-29 23:18:12 +0200

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='medical_chatbot',
+    version='0.1.0',
+    author='Arindam Roy',
+    author_email='ar98365@gmail.com',
+    packages=find_packages(),
+    install_requires=[]
+)
